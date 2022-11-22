8a4238cd507703c56b1b0abf46e23e46a08834d0
Hollie Fritz <hollie.fritz@bellevuecollege.edu>
2022-11-21 16:23:26 -0800

    Text layout change for homepage

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,6 +5,9 @@ import { useAuthenticator, Button } from '@aws-amplify/ui-react';
 import logo from './img/SmallNuOrderLogo.png';
 import "./Layout.css";
 
+import {ButtonGroup } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 export function Layout() {
   const { route, signOut } = useAuthenticator((context) => [
     context.route,
@@ -24,16 +27,18 @@ export function Layout() {
       </nav>
 
       <div className="I">
-      <h2 style={{color: "white"}}> I'M A </h2>
+      <h2 style={{color: "white"}}>I'm a </h2>
       </div>
       
       <div>
+      <ButtonGroup size="large">
         {route !== 'authenticated' ? (
           <Button style={{color: "orange"}} onClick={() => navigate('/login')}> Restaurateur </Button>
         ) : (
           <Button onClick={() => logOut()}> Logout </Button>
         )}
         <Button style={{color: "orange"}} onClick={() => navigate('/customer')}> Customer </Button>
+        </ButtonGroup>;
       </div>
       <Outlet />
     </>
