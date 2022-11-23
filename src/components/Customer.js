bbcc384e9d1df2462e466f1014f5de9d086aa873
Hollie Fritz <hollie.fritz@bellevuecollege.edu>
2022-11-22 23:16:26 -0800

    changed customer header

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,7 +4,7 @@ import "./Customer.css";
 export function Customer() {
     return (
       <div>
-        <h1 className="header-h1"> Let's find some food! </h1>
+        <h1 className="header-h1"> Let's get started! </h1>
         <form className="searchbar">
           <input type="text" placeholder="Search restaurant" name="search"/>
           <button type="submit"> Search </button>
