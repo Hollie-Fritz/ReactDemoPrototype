7fc99fbadff0bd1eb25fda8a304abe3a998a4922
Hollie Fritz <hollie.fritz@bellevuecollege.edu>
2023-02-13 13:21:12 -0800

    populate ResInfo for the ResReview page

--- src/owner/PersistResReview.js
+++ src/owner/PersistResReview.js
@@ -1,18 +1,19 @@
 import Card from "react-bootstrap/Card";
 
 //testing data populating from json
-function PersistResReview() {
+function PersistResReview({ formData }) {
+
   return (
     <div>
       <Card>
-        <Card.Header as="h1">resName</Card.Header>
+        <Card.Header as="h1">{formData.resName}</Card.Header>
         <Card.Body>
           <Card.Title>
-            Address: address1, address2, city, usstate, zip
+            Address: {formData.address1} {formData.address2}, {formData.city}, {formData.usstate}, {formData.zip}
           </Card.Title>
-          <Card.Text>Phone: phoneNo</Card.Text>
-          <Card.Text>Hours: resHours</Card.Text>
-          <Card.Text>Cuisine Type: resCuisine</Card.Text>
+          <Card.Text>Phone: 1-{formData.phoneNo}</Card.Text>
+          <Card.Text>Hours: {formData.openhours} - {formData.closehours}</Card.Text>
+          <Card.Text>Cuisine Type: {formData.resCuisine}</Card.Text>
         </Card.Body>
       </Card>
     </div>
