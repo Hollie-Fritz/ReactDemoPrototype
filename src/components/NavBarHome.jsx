086ca600fccb88318b850123785d3c48e66e59dd
kylemai96 <kylemai96@gmail.com>
2023-03-11 00:17:01 -0800

    fix nav bar (NavBarHome) to make action buttons work

--- src/components/NavBarHome.jsx
+++ src/components/NavBarHome.jsx
@@ -29,11 +29,10 @@ let NavBarHome = () => {
                   <Nav.Link href="/about">About</Nav.Link>
                   <Nav.Link href="/contact">Contacts</Nav.Link>
                   <NavDropdown title="Action" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">
-                      Login
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                      Sign Up
+                    <NavDropdown.Item onClick={() => {
+                    navigate("/login");
+                    }}>
+                      Login or Signup
                     </NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="/customer">
