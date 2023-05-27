2475746d08162837d0d0eaa5bb1aed8d8a0048a0
Jonida Durbaku <durbaku.j@gmail.com>
2023-05-27 14:45:26 -0700

    added footer and small changes on UX

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,14 @@ import React from "react";
 import "./Home.css";
 import {Customer} from "../components/customer/Customer";
 import { Container, Row } from "react-bootstrap";
+import FooterHomePage from "../components/footer/FooterHomePage";
 
 const Home = () => {
   return (
     <>
     <div className="home_bg_image">
-      <Container variant="sm">
-        <Row>
-          <Customer/>
-        </Row>
-      </Container>
+      <Customer/>
+      <FooterHomePage/>
     </div>
     </>
   );
