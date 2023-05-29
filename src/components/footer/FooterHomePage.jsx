ed9d994484ae2980c55193753957130237e84879
Jonida Durbaku <durbaku.j@gmail.com>
2023-05-28 22:52:29 -0700

    added icons on the footer and other small changes on other pages

--- src/components/footer/FooterHomePage.jsx
+++ src/components/footer/FooterHomePage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-
+import { FaGithubAlt, FaQuestion} from "react-icons/fa";
+import { AiTwotoneHome,AiTwotonePhone } from "react-icons/ai";
+import "./footer.css"
 let FooterHomePage = () => {
   return (
     <>
@@ -12,7 +14,7 @@ let FooterHomePage = () => {
               <h5>NuOrder</h5>
               <p>
                 We offer restaurant owners a website for free to present their
-                own restaurant page on the site as well as take in orders and
+                own restaurant page on the site as well as pick up orders and
                 requests. No prior experience is needed to use NuOrder for
                 creating the restaurant wesite.
               </p>
@@ -22,16 +24,19 @@ let FooterHomePage = () => {
               <h5>Links</h5>
               <ul className="list-unstyled">
                 <li>
-                  <a href="/">Home</a>
+                  <a href="/" className="link"><AiTwotoneHome size={18} /></a>
                 </li>
                 <li>
-                  <a href="/">Home</a>
+                  <a href="/about" className="link"><FaQuestion size={18} /></a>
                 </li>
                 <li>
-                  <a href="/about">About</a>
+                  <a href="/contact" className="link"><AiTwotonePhone size={18} /></a>
                 </li>
                 <li>
-                  <a href="/contact">Contact</a>
+                  <a 
+                  href="https://github.com/Hollie-Fritz/ReactDemoPrototype" 
+                  target="_blank"><FaGithubAlt size={18} 
+                  className="link"/></a>
                 </li>
               </ul>
             </Col>
