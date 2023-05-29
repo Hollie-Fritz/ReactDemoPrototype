e1102ae64aa359baea050347e43a47115a8b114f
Hollie Fritz <104112323+Hollie-Fritz@users.noreply.github.com>
2023-05-29 09:43:31 -0700

    make all cards uniform in size and add scroll add scroll bar for cards that are longer than the standard card change submit stage button color to match our pallette add comments

--- src/pages/ViewOrders.js
+++ src/pages/ViewOrders.js
@@ -1,19 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Auth from "@aws-amplify/auth";
 import React, { useState, useEffect } from "react";
-import { Card, Table, Container, Row, Form, Button, Col, Accordion, } from "react-bootstrap"; // prettier-ignore
+import { Card, Table, Container, Row, Form, Button, Col, } from "react-bootstrap"; // prettier-ignore
 import NavBarHome from "../components/NavBarHome";
 import OrderProgress from "./OrderProgress";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { useNavigate } from "react-router-dom";
 import { AiOutlineCheck } from "react-icons/ai";
 
+//component that displays the orders
 function ViewOrder() {
   const [orders, setOrders] = useState([]);
   const [loadingState, setLoadingState] = useState({});
   const { user } = useAuthenticator((context) => [context.user]);
   const navigate = useNavigate();
 
+  //fetches the list of orders
   async function fetchOrders() {
     const { username } = await Auth.currentUserInfo();
     const response = await fetch(
@@ -26,11 +28,13 @@ function ViewOrder() {
     setOrders(data);
   }
 
+  //update the status of the orders (progress bar)
   async function updateOrderStatus(orderId, status) {
     setLoadingState((prevState) => ({ ...prevState, [orderId]: true }));
 
     const body = { orderId, updateKey: "progress", updateValue: status };
 
+    //send a PATCH request to the API endpoint to update the status of the order
     await fetch(
       "https://6b2uk8oqk7.execute-api.us-west-2.amazonaws.com/prod/order",
       {
@@ -53,6 +57,7 @@ function ViewOrder() {
     fetchOrders();
   }, []);
 
+  //rendering the component
   return (
     <>
       <NavBarHome />
@@ -73,7 +78,7 @@ function ViewOrder() {
     </>
   );
 }
-
+//subcomponent that displays single order
 function OrderCard({
   order,
   index,
@@ -86,119 +91,114 @@ function OrderCard({
   const orderDateTime = new Date(order["dateTime"]);
 
   return (
-    <Col md={5}>
-      <Accordion defaultActiveKey="0">
-        <Card className="mb-3">
-          <Card.Body>
-            <Card.Title>
-              <nobr className="fw-bold">{order["customerName"]}'s order</nobr>
-            </Card.Title>
-            <Card.Text>
-              <nobr className="fw-bold">Order Date:</nobr>{" "}
-              {orderDateTime.toLocaleDateString()}
-              <br></br>
-              <nobr className="fw-bold">Order Time:</nobr>{" "}
-              {orderDateTime.toLocaleTimeString()}
-              <Table responsive>
-                <thead>
-                  <tr>
-                    <th>Item</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {order["menuItems"].map((current) => {
-                    return (
-                      <tr key={current["foodName"]}>
-                        <td>{current["foodName"]}</td>
-                        <td>{current["quantity"]}</td>
-                        <td>${current["priceEach"]}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </Table>
-              <nobr
-                className="fw-bold border-0"
-                style={{ background: "white" }}
-              >
-                {" "}
-                Note: {order["note"]}
-              </nobr>
-              <br></br>
-              <nobr
-                className="fw-bold border-0"
-                style={{ background: "white" }}
-              >
-                {" "}
-                Utensils:{" "}
-              </nobr>
-              {order["utensils"] ? "yes" : "no"}
-              <br></br>
-              <nobr
-                className="fw-bold border-0"
-                style={{ background: "white" }}
-              >
-                {" "}
-                Total Cost:{" "}
-              </nobr>{" "}
-              ${order["totalCost"]}
-              <br></br>
-              <Form.Label className="fw-bold" column sm="2">
-                Progress:
-              </Form.Label>
-              <Form.Group as={Row}>
-                <Col>
-                  <Form.Select
-                    className="form-control"
-                    name="progress"
-                    defaultValue={order["progress"]}
-                    id={"form" + index}
-                  >
-                    <option value="Order placed"> Order Placed </option>
-                    <option value="Preparing"> Preparing </option>
-                    <option value="Ready"> Ready </option>
-                  </Form.Select>
-                </Col>
-                <Col sm="4">
-                  <Button
-                    onClick={async () => {
-                      const form = document.getElementById("form" + index);
-                      await updateOrderStatus(order["id"], form.value);
-                    }}
-                    style={{ marginBottom: "20px" }}
-                  >
-                    {loadingState[order["id"]] ? (
-                      <AiOutlineCheck size={20} />
-                    ) : (
-                      "Submit Stage"
-                    )}
-                  </Button>
-                </Col>
-              </Form.Group>
-              <OrderProgress stage={order["progress"]} />
-              {user && order["customerId"] !== "" && (
-                <Button
-                  style={{ marginTop: "20px" }}
-                  onClick={() =>
-                    navigate(
-                      `/chat/${user.getUsername()}/${order["customerId"]}`,
-                      {
-                        state: {
-                          name: order["customerName"],
-                        },
-                      }
-                    )
-                  }
+    <Col md={5} style={{ margin: "30px" }}>
+      <Card className="mb-3" style={{ height: "456px", overflowY: "auto" }}>
+        <Card.Body>
+          <Card.Title>
+            <nobr className="fw-bold">{order["customerName"]}'s order</nobr>
+          </Card.Title>
+          <Card.Text>
+            <nobr className="fw-bold">Order Date:</nobr>{" "}
+            {orderDateTime.toLocaleDateString()}
+            <br></br>
+            <nobr className="fw-bold">Order Time:</nobr>{" "}
+            {orderDateTime.toLocaleTimeString()}
+            <Table responsive>
+              <thead>
+                <tr>
+                  <th>Item</th>
+                  <th>Quantity</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {order["menuItems"].map((current) => {
+                  return (
+                    <tr key={current["foodName"]}>
+                      <td>{current["foodName"]}</td>
+                      <td>{current["quantity"]}</td>
+                      <td>${current["priceEach"]}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+            <nobr className="fw-bold border-0" style={{ background: "white" }}>
+              {" "}
+              Note: {order["note"]}
+            </nobr>
+            <br></br>
+            <nobr className="fw-bold border-0" style={{ background: "white" }}>
+              {" "}
+              Utensils:{" "}
+            </nobr>
+            {order["utensils"] ? "yes" : "no"}
+            <br></br>
+            <nobr className="fw-bold border-0" style={{ background: "white" }}>
+              {" "}
+              Total Cost:{" "}
+            </nobr>{" "}
+            ${order["totalCost"]}
+            <br></br>
+            <Form.Label className="fw-bold" column sm="2">
+              Progress:
+            </Form.Label>
+            <Form.Group as={Row}>
+              <Col>
+                <Form.Select
+                  className="form-control"
+                  name="progress"
+                  defaultValue={order["progress"]}
+                  id={"form" + index}
                 >
-                  Chat
+                  <option value="Order placed"> Order Placed </option>
+                  <option value="Preparing"> Preparing </option>
+                  <option value="Ready"> Ready </option>
+                </Form.Select>
+              </Col>
+              <Col sm="4">
+                <Button
+                  onClick={async () => {
+                    // retrieve element of form using unique id.
+                    const form = document.getElementById("form" + index);
+                    await updateOrderStatus(order["id"], form.value);
+                  }}
+                  style={{
+                    marginBottom: "20px",
+                    backgroundColor: "#212529",
+                    color: "white",
+                  }}
+                >
+                  {loadingState[order["id"]] ? (
+                    //checkmark
+                    <AiOutlineCheck size={20} />
+                  ) : (
+                    "Submit Stage"
+                  )}
                 </Button>
-              )}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Accordion>
+              </Col>
+            </Form.Group>
+            <OrderProgress stage={order["progress"]} />
+            {user && order["customerId"] !== "" && (
+              <Button
+                style={{ marginTop: "20px" }}
+                onClick={() =>
+                  navigate(
+                    `/chat/${user.getUsername()}/${order["customerId"]}`,
+                    {
+                      state: {
+                        name: order["customerName"],
+                      },
+                    }
+                  )
+                }
+              >
+                Chat
+              </Button>
+            )}
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </Col>
   );
 }
