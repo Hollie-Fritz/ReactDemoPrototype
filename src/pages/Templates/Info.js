47171160e4aeefa961d513f572c8157c6e426cf3
Giang Ngo <gnngo2502@gmail.com>
2023-06-01 20:00:36 -0700

    fix the hours status bug

--- src/pages/Templates/Info.js
+++ src/pages/Templates/Info.js
@@ -1,78 +1,80 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 import AverageRating from "../../components/rating/AverageRating";
-import moment from "moment-timezone"
+import moment from "moment-timezone";
 
-function Info(props){
-    const {resdata, averageRating} = props.data;
-    const [isOpen, setIsOpen] = useState(false);
+function Info(props) {
+  const { resdata, averageRating } = props.data;
+  const [isOpen, setIsOpen] = useState(false);
 
-    useEffect(() => {
+  useEffect(() => {
+    const checkIsOpen = () => {
+      if (resdata.openHours && resdata.closeHours) {
+        const now = moment().tz("America/Los_Angeles");
+        let openingTime = moment.tz(
+          `${now.format("YYYY-MM-DD")} ${resdata.openHours}`,
+          "YYYY-MM-DD hh:mm A",
+          "America/Los_Angeles"
+        );
+        let closingTime = moment.tz(
+          `${now.format("YYYY-MM-DD")} ${resdata.closeHours}`,
+          "YYYY-MM-DD hh:mm A",
+          "America/Los_Angeles"
+        );
 
-    
-        let openingTime, closingTime;
-    
-        if (resdata.openHours && resdata.closeHours) {
-          openingTime = moment.tz(resdata.openHours, "hh:mm A", 'America/Los_Angeles');
-          closingTime = moment.tz(resdata.closeHours, "hh:mm A", 'America/Los_Angeles');
-    
-          if (closingTime.isBefore(openingTime)) {
-            closingTime = closingTime.add(1, 'days');
-          }
+        console.log("Now:", now.format("hh:mm A")); // This will log the current time
+        console.log("Opening Time:", openingTime.format("hh:mm A")); // This will log the opening time
+        console.log("Closing Time:", closingTime.format("hh:mm A")); // This will log the closing time
+
+        if (closingTime.isBefore(openingTime)) {
+          closingTime = closingTime.add(1, "days");
         }
-    
-        const checkIsOpen = () => {
-          const now = moment().tz('America/Los_Angeles'); // Move the `now` constant here
-    
-          console.log('Now:', now.format("hh:mm A")); // This will log the current time
-          console.log('Opening Time:', openingTime.format("hh:mm A")); // This will log the opening time
-          console.log('Closing Time:', closingTime.format("hh:mm A")); // This will log the closing time
-    
-          return now.isBetween(openingTime, closingTime, undefined, '[]');
-        };
 
-        setIsOpen(checkIsOpen());
-        
-        const interval = setInterval(() => setIsOpen(checkIsOpen()), 60000);
-        return () => clearInterval(interval);
-      }, [resdata]);
-    
+        setIsOpen(now.isBetween(openingTime, closingTime, undefined, "[]"));
+      }
+    };
 
-return(   
-    <>     
-    {/* restaurant address */}
-    <Card.Text>
-    <nobr className="fw-bold">Address: </nobr>
-        {resdata["address1"]} {resdata["address2"]},{" "}
-        {resdata["city"]}, {resdata["state"]}{" "}
-        {resdata["zipCode"]}
-    </Card.Text>
-    {/* restaurant phone number */}
-    <Card.Text>
+    checkIsOpen();
+    const interval = setInterval(checkIsOpen, 60000);
+    return () => clearInterval(interval);
+  }, [resdata]);
+
+  return (
+    <>
+      {/* restaurant address */}
+      <Card.Text>
+        <nobr className="fw-bold">Address: </nobr>
+        {resdata["address1"]} {resdata["address2"]}, {resdata["city"]},{" "}
+        {resdata["state"]} {resdata["zipCode"]}
+      </Card.Text>
+      {/* restaurant phone number */}
+      <Card.Text>
         <nobr className="fw-bold">Phone: </nobr>
         {resdata["phone"]}
-    </Card.Text>
-    {/* restaurant hours */}
-    <Card.Text>
+      </Card.Text>
+      {/* restaurant hours */}
+      <Card.Text>
         <nobr className="fw-bold">Hours: </nobr>
         {resdata["openHours"]} - {resdata["closeHours"]}
-    </Card.Text>
-    <Card.Text>
+      </Card.Text>
+      <Card.Text>
+        {console.log('isOpen:', isOpen)} {/* test status */}
         {isOpen ? "We are currently open!" : "We are currently closed."}
-    </Card.Text>
+      </Card.Text>
 
-    {/* cuisine type */}
-    <Card.Text>
+      {/* cuisine type */}
+      <Card.Text>
         <nobr className="fw-bold">Cuisine Type: </nobr>
         {resdata["cuisine"]}
-    </Card.Text>
-    <Card.Text>
+      </Card.Text>
+      <Card.Text>
         <div style={{ display: "flex", alignItems: "center" }}>
-            <span className="fw-bold">Rating: </span>
-            <AverageRating averageRating={averageRating} />
+          <span className="fw-bold">Rating: </span>
+          <AverageRating averageRating={averageRating} />
         </div>
-    </Card.Text></>
-)
+      </Card.Text>
+    </>
+  );
 }
 
 export default Info;
