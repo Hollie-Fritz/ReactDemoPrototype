2ba78164095130cabeddbbeabc3f01133b303541
Hollie Fritz <104112323+Hollie-Fritz@users.noreply.github.com>
2023-06-11 13:34:06 -0700

    fix template1 images to be on the right of the menu items fix sign up dropdown to be on left

--- src/components/NavBarHome.jsx
+++ src/components/NavBarHome.jsx
@@ -10,7 +10,7 @@ let NavBarHome = () => {
   const { signOut, user } = useAuthenticator((context) => [
     context.route,
     context.signOut,
-    context.user,
+    context.user
   ]);
   const navigate = useNavigate();
 
@@ -21,18 +21,14 @@ let NavBarHome = () => {
     navigate(-1);
   }
 
-
   return (
     <>
-
-        <Navbar bg="dark" expand="lg" variant="dark" style={{fontSize: "22px"}}>
+      <Navbar bg="dark" expand="lg" variant="dark" style={{ fontSize: "22px" }}>
         <Container className="mt-1" fluid>
           <Row>
             <Col md="auto">
               <Navbar.Brand>
-                <Nav.Link
-                  href="/"
-                >
+                <Nav.Link href="/">
                   <img src={logo} alt="logo" className="logoHome" />
                 </Nav.Link>
               </Navbar.Brand>
@@ -41,50 +37,78 @@ let NavBarHome = () => {
               <Navbar.Toggle aria-controls="navbarScroll" />
               <Navbar.Collapse id="navbarScroll">
                 <Nav className="mr-auto">
-                  <Nav.Link href="/about" style={{fontWeight: "bold"}}>About</Nav.Link>
-                  <Nav.Link href="/contact" style={{fontWeight: "bold"}}>Contact</Nav.Link>
-                  {user?
-                  <>
-                  <Nav.Link 
-                    href="/orderStatus"  
-                    style={{fontWeight: "bold"}}
-                  > My Orders </Nav.Link>
-                  <Nav.Link
-                    href="/owner"
-                    style={{fontWeight: "bold"}}
-                  > Merchant Portal </Nav.Link>
-                  <Nav.Link 
-                    href="/chat"
-                    style={{fontWeight: "bold", position: "absolute", right: 200}}
-                  > Chat </Nav.Link>
-                    <NavDropdown
-                    title= {user.username}
-                    id="basic-nav-dropdown"
-                    className="fw-bold" style={{ position: "absolute", right: 15, fontSize: "20px", color: "#fff" }}
-                  >
-                    <NavDropdown.Item
+                  <Nav.Link href="/about" style={{ fontWeight: "bold" }}>
+                    About
+                  </Nav.Link>
+                  <Nav.Link href="/contact" style={{ fontWeight: "bold" }}>
+                    Contact
+                  </Nav.Link>
+                  {user ? (
+                    <>
+                      <Nav.Link
+                        href="/orderStatus"
+                        style={{ fontWeight: "bold" }}
+                      >
+                        {" "}
+                        My Orders{" "}
+                      </Nav.Link>
+                      <Nav.Link href="/owner" style={{ fontWeight: "bold" }}>
+                        {" "}
+                        Merchant Portal{" "}
+                      </Nav.Link>
+                      <Nav.Link
+                        href="/chat"
+                        style={{
+                          fontWeight: "bold",
+                          position: "absolute",
+                          right: 200
+                        }}
+                      >
+                        {" "}
+                        Chat{" "}
+                      </Nav.Link>
+                      <NavDropdown
+                        title={user.username}
+                        id="basic-nav-dropdown"
+                        className="fw-bold dropdown"
+                        style={{
+                          position: "absolute",
+                          right: 15,
+                          fontSize: "20px",
+                          color: "#fff"
+                        }}
+                      >
+                        <NavDropdown.Item
                           onClick={() => {
-                            logOut()
-                            }} style={{fontWeight: "bold"}}
+                            logOut();
+                          }}
+                          style={{ fontWeight: "bold" }}
                         >
-                        Sign Out
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                  </>
-                  :
-                  <Nav.Link onClick={() => {
-                    navigate("/login");
-                    }} style={{position: "absolute", right: 15, fontWeight: "bold"}}
-                    > Signup/Login </Nav.Link>
-                  }
-
+                          Sign Out
+                        </NavDropdown.Item>
+                      </NavDropdown>
+                    </>
+                  ) : (
+                    <Nav.Link
+                      onClick={() => {
+                        navigate("/login");
+                      }}
+                      style={{
+                        position: "absolute",
+                        right: 15,
+                        fontWeight: "bold"
+                      }}
+                    >
+                      {" "}
+                      Signup/Login{" "}
+                    </Nav.Link>
+                  )}
                 </Nav>
               </Navbar.Collapse>
             </Col>
           </Row>
-          </Container>
-        </Navbar>
-
+        </Container>
+      </Navbar>
     </>
   );
 };
--- src/pages/Templates/Menu.js
+++ src/pages/Templates/Menu.js
@@ -1,82 +1,92 @@
 import React from "react";
-import { Card, Row, Col, Button} from "react-bootstrap";
+import { Card, Row, Col, Button } from "react-bootstrap";
 
-function Menu(props){
-    const {groupedFoodData, handleAddClick, showAddedMessage, cart} = props.data;
+function Menu(props) {
+  const { groupedFoodData, handleAddClick, showAddedMessage, cart } = props.data;
 
-return(
+  return (
     <>
-     {Object.entries(groupedFoodData).map(
-        ([foodType, foodItems]) => {
-          return (
-            <div key={foodType}>
-              <h2>{foodType}</h2>
-              <br></br><br></br>
-              <Row md={1} lg={3} className="g-4">
-                {foodItems.map((item, index) => {
-                  return (
-                    <Col className="d-flex align-items-stretch">
-                      {/* inner card three */}
-                      <Card key={index} style={{ width: "37rem" }}>
-                        <Card.Img
-                          variant="top"
-                          src={
-                            item.foodImageUrl
-                              ? "https://d12zok1slvqtin.cloudfront.net/fit-in/286x180/" +
+      {Object.entries(groupedFoodData).map(([foodType, foodItems]) => {
+        return (
+          <div key={foodType}>
+            <h2>{foodType}</h2>
+            <br />
+            <br />
+            <Row md={1} lg={3} className="g-4">
+              {foodItems.map((item, index) => {
+                return (
+                  <Col className="d-flex align-items-stretch">
+                    {/* inner card three */}
+                    <Card key={index} style={{ width: "37rem" }}>
+                      <Card.Body>
+                        <Row>
+                          <Col md={8}>
+                            <Card.Text
+                              style={{
+                                fontSize: "18px",
+                                wordWrap: "break-word",
+                                overflowWrap: "break-word",
+                              }}
+                              as="h5"
+                              className="text-center font-size: 10px"
+                            >
+                              <div as="h1" className="fw-bold">
+                                {item.foodName}
+                              </div>
+                              <Card.Text>${item.foodPrice}</Card.Text>
+                              <Card.Text>{item.foodDesc}</Card.Text>
+                            </Card.Text>
+                            <Card.Footer
+                              className="border-0"
+                              style={{ background: "white" }}
+                            >
+                              <br />
+                              <br />
+                              {/* Add to the cart button */}
+                              <Button
+                                style={{
+                                  position: "absolute",
+                                  bottom: 5,
+                                  left: 5,
+                                }}
+                                onClick={() => handleAddClick(item.foodId)}
+                              >
+                                {showAddedMessage === item.foodId &&
+                                cart[item.foodId] > 0
+                                  ? "✓"
+                                  : "Add"}
+                              </Button>
+                            </Card.Footer>
+                          </Col>
+                          <Col md={4}>
+                            <Card.Img
+                              variant="top"
+                              src={
                                 item.foodImageUrl
-                              : ""
-                          }
-                        />
-                        <Card.Body>
-                          <Card.Text
-                          style={{ fontSize: "18px", wordWrap: "break-word", overflowWrap: "break-word" }}
-                            as="h5"
-                            className="text-center font-size: 10px"
-                          >
-                            <div as="h1" className="fw-bold">
-                              {item.foodName}
-                            </div>
-                          </Card.Text>
-                          <Card.Text>${item.foodPrice}</Card.Text>
-                          <Card.Text>{item.foodDesc}</Card.Text>
-                        </Card.Body>
-                        <Card.Footer
-                          className="border-0"
-                          style={{ background: "white" }}
-                        >
-                          <br></br>
-                          <br></br>
-                          {/* Add to the cart button */}
-                          <Button
-                            style={{
-                              position: "absolute",
-                              bottom: 5,
-                              left: 5,
-                            }}
-                            onClick={() =>
-                              handleAddClick(item.foodId)
-                            }
-                          >
-                            {showAddedMessage === item.foodId &&
-                            cart[item.foodId] > 0
-                              ? "✓"
-                              : "Add"}
-                          </Button>
-                        </Card.Footer>
-                        {/* end inner card three */}
-                      </Card>
-                    </Col>
-                  );
-                })}
-                
-              </Row>
-              <br></br><br></br>
-            </div>
-          );
-        }
-      )}
-      </>
-)
-    }
-    
+                                  ? "https://d12zok1slvqtin.cloudfront.net/fit-in/286x180/" +
+                                    item.foodImageUrl
+                                  : ""
+                              }
+                              style={{objectFit: "cover",
+                            height: "100%",
+                          width: "100%"}}
+                            />
+                          </Col>
+                        </Row>
+                      </Card.Body>
+                    </Card>
+                    {/* end inner card three */}
+                  </Col>
+                );
+              })}
+            </Row>
+            <br />
+            <br />
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
 export default Menu;
