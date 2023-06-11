9fe084814281781ca69bd2fee471f3d552e545c5
Hollie Fritz <104112323+Hollie-Fritz@users.noreply.github.com>
2023-06-11 14:05:42 -0700

    add design login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useEffect } from "react";
 
-import { Authenticator, useAuthenticator, View } from "@aws-amplify/ui-react";
+import { Authenticator, useAuthenticator, View, useTheme, Image, Text, Heading, Button } from "@aws-amplify/ui-react";
 
 import "@aws-amplify/ui-react/styles.css";
+import logo from "../assests/NuOrderLogoLarge.png";
+
 
 import { useNavigate, useLocation } from "react-router";
 import NavBarHome from "./NavBarHome";
@@ -21,13 +23,239 @@ export function Login() {
     }
   }, [route, navigate, from]);
 
+  const components = {
+    Header() {
+      const { tokens } = useTheme();
+
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Image src={logo} alt="logo" className="logoHome" />
+        </View>
+      );
+    },
+
+    Footer() {
+      const { tokens } = useTheme();
+
+      return (
+        <View textAlign="center" padding={tokens.space.large}>
+          <Text color={tokens.colors.neutral[80]}>
+            &copy; All Rights Reserved
+          </Text>
+        </View>
+      );
+    },
+
+    SignIn: {
+      Header() {
+        const { tokens } = useTheme();
+
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Sign in to your account
+          </Heading>
+        );
+      },
+      Footer() {
+        const { toResetPassword } = useAuthenticator();
+
+        return (
+          <View textAlign="center">
+            <Button
+              fontWeight="normal"
+              onClick={toResetPassword}
+              size="small"
+              variation="link"
+            >
+              Reset Password
+            </Button>
+          </View>
+        );
+      },
+    },
+
+    SignUp: {
+      Header() {
+        const { tokens } = useTheme();
+
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Create a new account
+          </Heading>
+        );
+      },
+      Footer() {
+        const { toSignIn } = useAuthenticator();
+
+        return (
+          <View textAlign="center">
+            <Button
+              fontWeight="normal"
+              onClick={toSignIn}
+              size="small"
+              variation="link"
+            >
+              Back to Sign In
+            </Button>
+          </View>
+        );
+      },
+    },
+    ConfirmSignUp: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+    SetupTOTP: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+    ConfirmSignIn: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+    ResetPassword: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+    ConfirmResetPassword: {
+      Header() {
+        const { tokens } = useTheme();
+        return (
+          <Heading
+            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
+            level={3}
+          >
+            Enter Information:
+          </Heading>
+        );
+      },
+      Footer() {
+        return <Text>Footer Information</Text>;
+      },
+    },
+  };
+
+  const formFields = {
+    signIn: {
+      username: {
+        placeholder: 'Enter your email',
+      },
+    },
+    signUp: {
+      password: {
+        label: 'Password:',
+        placeholder: 'Enter your Password:',
+        isRequired: false,
+        order: 2,
+      },
+      confirm_password: {
+        label: 'Confirm Password:',
+        order: 1,
+      },
+    },
+    forceNewPassword: {
+      password: {
+        placeholder: 'Enter your Password:',
+      },
+    },
+    resetPassword: {
+      username: {
+        placeholder: 'Enter your email:',
+      },
+    },
+    confirmResetPassword: {
+      confirmation_code: {
+        placeholder: 'Enter your Confirmation Code:',
+        label: 'New Label',
+        isRequired: false,
+      },
+      confirm_password: {
+        placeholder: 'Enter your Password Please:',
+      },
+    },
+    setupTOTP: {
+      QR: {
+        totpIssuer: 'test issuer',
+        totpUsername: 'amplify_qr_test_user',
+      },
+      confirmation_code: {
+        label: 'New Label',
+        placeholder: 'Enter your Confirmation Code:',
+        isRequired: false,
+      },
+    },
+    confirmSignIn: {
+      confirmation_code: {
+        label: 'New Label',
+        placeholder: 'Enter your Confirmation Code:',
+        isRequired: false,
+      },
+    },
+  };
+
   return (
     <>
     <NavBarHome/>
     <br></br>
     <View className="auth-wrapper">
       {/* Sign-in box */}
-      <Authenticator></Authenticator>
+      <Authenticator formFields={formFields} components={components}>
+      {({ signOut }) => <button onClick={signOut}>Sign out</button>}
+    </Authenticator>
       {/* User note */}
       <p style={{textAlign: 'center', marginTop: '10px'}}>
       <nobr className="fw-bold">Note:</nobr> Your username will be used as the URL of your page.
